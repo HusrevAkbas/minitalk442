b126eb646b350468c6ab17e5a4f4b5a3b42e70fa
Husrev Akbas <huakbas@c4r3p5.42vienna.com>
2024-12-24 12:33:38 +0100

    clear list function changed

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huakbas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:10:55 by huakbas           #+#    #+#             */
-/*   Updated: 2024/09/23 17:44:23 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/12/24 12:33:01 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst == NULL || del == NULL)
 		return ;
-	del(lst->content);
+	del(lst);
 	free(lst);
 	lst = NULL;
 }
