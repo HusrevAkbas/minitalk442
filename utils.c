9e9a9fb7bf74a1bfb6dfce769c9170530958a72d
Husrev Akbas <huakbas@c4r3p5.42vienna.com>
2024-12-24 11:47:08 +0100

    init string function added

--- utils.c
+++ utils.c
@@ -6,8 +6,29 @@
 /*   By: huakbas <huakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:25:38 by huakbas           #+#    #+#             */
-/*   Updated: 2024/12/20 16:27:13 by huakbas          ###   ########.fr       */
+/*   Updated: 2024/12/24 11:40:18 by huakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+t_stringholder	*init_string()
+{
+	t_stringholder	*string;
+
+	string = malloc(sizeof(t_stringholder));
+	if (!string)
+		return (NULL);
+	string->str = ft_calloc(BUFF_SIZE + 1, sizeof(char));
+	if (!string->str)
+	{
+		free(string);
+		return (NULL);
+	}
+	ft_bzero(string->bin, 9);
+	string->i_bin = 0;
+	string->i_str = 0;
+	string->is_long = 0;
+	string->is_long_set = 0;
+	return (string);
+}
